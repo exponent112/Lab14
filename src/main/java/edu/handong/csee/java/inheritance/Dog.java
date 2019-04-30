926b733445867d2eb97164aad9947449d27dfa3d
exponent112 <21800181@handong.edu>
2019-04-30 13:52:36 +0900

    plus Dog

--- src/main/java/edu/handong/csee/java/inheritance/Dog.java
+++ src/main/java/edu/handong/csee/java/inheritance/Dog.java
@@ -1,5 +1,10 @@
 package edu.handong.csee.java.inheritance;
 
 public class Dog extends Animal {
-	
+	@Override
+	public void testInstanceMethod() {
+		// TODO Auto-generated method stub
+		//super.testInstanceMethod();
+		System.out.println("testInstamnceMethod in Dog");
+	}
 }
